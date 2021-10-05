df4c49ade1c8b42fac42072000c517f197bdbb36
tulio-amaral <tuliosantos@gee.inatel.br>
2021-10-05 10:43:25 -0300

    create header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Flex, Button, Stack,  } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
@@ -18,35 +19,11 @@ export default function Home() {
         flexDir="column"
       >
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-              <Input
-                name="email"
-                type="email" 
-                focusBorderColor="pink.500"
-                bgColor="gray.900"
-                variant="filled"
-                _hover={{
-                  bgColor: 'gray.900'
-                }}
-                size='lg'
-              />
-          </FormControl>
-          
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-              <Input
-                name="password"
-                type="password" 
-                focusBorderColor="pink.500"
-                bgColor="gray.900"
-                variant="filled"
-                _hover={{
-                  bgColor: 'gray.900'
-                }}
-                size='lg'
-              />
-          </FormControl>
+
+          <Input name="email" type="email" label="E-mail" />
+
+          <Input name="password" type="password" label="Password" />
+
           <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
         </Stack>
       </Flex>
